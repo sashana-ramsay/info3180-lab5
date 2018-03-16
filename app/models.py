944fde0a8cc3580fa2da7a1a0f0385c889d6a24b
sashana <sashana.ramsay@mymona.uwi.edu>
2018-03-16 19:46:22 +0000

    deleted commits

--- app/models.py
+++ app/models.py
@@ -6,8 +6,8 @@ class UserProfile(db.Model):
     first_name = db.Column(db.String(80))
     last_name = db.Column(db.String(80))
     username = db.Column(db.String(80), unique=True)
-    password = db.Column(db.String(255))
-
+    password = db.Column(db.String(250))
+    
     def is_authenticated(self):
         return True
 
